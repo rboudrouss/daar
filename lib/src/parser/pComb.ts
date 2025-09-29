5c7847ab5523402453e26be3e35f9910fe6c59d0
rboudrouss <r.boudrouss@gmail.com>
2025-09-29 10:01:41 +0200

    feat: add parser library

---
+++ lib/src/parser/pComb.ts
@@ -0,0 +1,89 @@
+import Parser, { ParserTuple } from "./parser.ts";
+import ParserState from "./pState.ts";
+
+/** Takes an array of parsers, and returns a new parser that matches each of them sequentially, collecting up the results into an array. */
+export const sequence = <R extends any[], D>(parsers: ParserTuple<R, D>) =>
+  new Parser((s) => {
+    if (s.error) return s;
+    const results = [];
+    let nextState = s;
+
+    for (const parser of parsers) {
+      const out = parser.pf(nextState);
+      if (out.error) return results ? out.updateResult(results) : out;
+      nextState = out;
+      results.push(out.result);
+    }
+    return nextState.updateResult(results);
+  }) as Parser<R, D>;
+
+export const many = function many<T>(parser: Parser<T>): Parser<T[]> {
+  return new Parser(function many$state(state) {
+    if (state.isError) return state;
+
+    const results = [];
+    let nextState = state;
+
+    while (true) {
+      const out = parser.pf(nextState);
+
+      if (out.isError) {
+        break;
+      } else {
+        nextState = out;
+        results.push(nextState.result);
+
+        if (nextState.index >= nextState.dataView.byteLength) {
+          break;
+        }
+      }
+    }
+
+    return nextState.updateResult(results);
+  });
+};
+
+export function possibly<T, D>(parser: Parser<T, D>): Parser<T | null, D> {
+  return new Parser(function possibly$state(state) {
+    if (state.isError) return state;
+
+    const nextState = parser.pf(state);
+    return nextState.isError ? state.updateResult(null) : nextState;
+  });
+}
+
+type ParserFn<T> = (_yield: <K>(parser: Parser<K>) => K) => T;
+
+export function coroutine<T>(parserFn: ParserFn<T>): Parser<T> {
+  return new Parser(function coroutine$state(state) {
+    let currentValue;
+    let currentState = state;
+
+    const run = <T>(parser: Parser<T>) => {
+      if (!(parser && parser instanceof Parser)) {
+        throw new Error(
+          `[coroutine] passed values must be Parsers, got ${parser}.`
+        );
+      }
+      const newState = parser.pf(currentState);
+      if (newState.isError) {
+        throw newState;
+      } else {
+        currentState = newState;
+      }
+      currentValue = currentState.result;
+      return currentValue;
+    };
+
+    try {
+      const result = parserFn(run);
+      return currentState.updateResult(result);
+    } catch (e) {
+      if (e instanceof Error) {
+        throw e;
+      } else {
+        return e as ParserState<any, any>;
+      }
+    }
+  });
+}
---
+++ lib/src/parser/pGen.ts
@@ -0,0 +1,92 @@
+import Parser, { ParsingFunction } from "./parser.ts";
+
+let decoder = new TextDecoder("utf-8");
+let encoder = new TextEncoder();
+
+export const str = (s: string) => {
+  if (!(s && typeof s === "string"))
+    throw new TypeError(
+      `[str] must be called with a string with strict positive length, got ${s}`
+    );
+
+  let es = encoder.encode(s);
+
+  return new Parser(((PS) => {
+    if (PS.error) return PS;
+    const { index, dataView } = PS;
+    const remains = dataView.byteLength - index;
+    if (!remains)
+      return PS.updateError(`[str] Expected '${s}' got End Of Input`);
+
+    if (remains < es.byteLength) {
+      let sai = PS.getString(index, remains);
+
+      return PS.updateError(`[str] Expected '${s}' got '${sai}'`);
+    }
+    let sai = PS.getString(index, es.byteLength);
+
+    let out =
+      s === sai
+        ? PS.updateByteIndex(es.byteLength).updateResult(s)
+        : PS.updateError(`[str] Expected '${s}' got '${sai}'`);
+
+    return out;
+  }) as ParsingFunction<string, null>);
+};
+
+export const fail = (e: string) => new Parser((s) => s.updateError(e));
+
+export const succeed = <T>(e: T) => new Parser((s) => s.updateResult(e));
+
+export function regex(re: RegExp): Parser<string> {
+  const typeofre = Object.prototype.toString.call(re);
+  if (typeofre !== "[object RegExp]") {
+    throw new TypeError(
+      `regex must be called with a Regular Expression, but got ${typeofre}`
+    );
+  }
+
+  if (re.toString()[1] !== "^") {
+    throw new Error(`regex parsers must contain '^' start assertion.`);
+  }
+
+  return new Parser(function regex$state(state) {
+    if (state.isError) return state;
+    const { dataView, index } = state;
+    const rest = state.getString(index, dataView.byteLength - index);
+
+    if (rest.length >= 1) {
+      const match = rest.match(re);
+      return match
+        ? state
+            .updateResult(match[0])
+            .updateByteIndex(encoder.encode(match[0]).byteLength)
+        : state.updateError(
+            `ParseError (position ${index}): Expecting string matching '${re}', got '${rest.slice(
+              0,
+              5
+            )}...'`
+          );
+    }
+    return state.updateError(
+      `ParseError (position ${index}): Expecting string matching '${re}', but got end of input.`
+    );
+  });
+}
+
+export const logState = (msg = "") =>
+  new Parser((PS) => {
+    console.log("logState : ", msg);
+    console.log(PS);
+    return PS;
+  });
+
+export const getIndex = new Parser((s) =>
+  s.updateResult({
+    bitIndex: s.bitIndex,
+    index: s.index,
+  })
+);
+
+export const addIndex = (n: number) =>
+  new Parser((s) => (s.isError ? s : s.updateByteIndex(n)));

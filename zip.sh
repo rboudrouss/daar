51c0dda4d0e534154963d90d985c64e12e0ca0cc
rboudrouss <r.boudrouss@gmail.com>
2025-10-19 22:18:38 +0200

    feat: update zip script

--- zip.sh
+++ zip.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 cp report/projet1/rapport.pdf ./DAAR-Projet1-Rapport-Boudrouss-Breton.pdf
-7z a DAAR-Projet1-Boudrouss-Breton.zip report/* cli/* lib/* data/* pnpm-lock.yaml pnpm-workspace.yaml tsconfig.base.json tsconfig.json .gitignore package.json README.md egrep DAAR-Projet1-Rapport-Boudrouss-Breton.pdf -xr!lib/dist -xr!lib/node_modules -xr!cli/node_modules
+7z a daar-projet-offline-BOUDROUSS-BRETON.zip report/* cli/* lib/* data/* pnpm-lock.yaml pnpm-workspace.yaml tsconfig.base.json tsconfig.json .gitignore package.json README.md egrep DAAR-Projet1-Rapport-Boudrouss-Breton.pdf -xr!lib/dist -xr!lib/node_modules -xr!cli/node_modules
